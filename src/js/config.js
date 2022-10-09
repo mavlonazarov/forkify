e4752f510b8c05aa022d7084465f65754c52f55a
lordskiy <dalermavlonazarov@Daler.local>
2022-10-10 00:17:01 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9377081e-7e5d-4e8c-b79d-fc5e8eedb0a2';
+export const MODAL_CLOSE_SEC = 2.5;
